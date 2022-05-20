a614bdf5e3fd24830c69a5ce2c1b695d25fdadc5
Jakob Reid <jakob.reid@live.com>
2022-05-20 16:06:30 +1000

    github logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import GitHub from "../assets/github.svg";
 
 function Footer() {
     const getCurrentYear = ()=> {
@@ -7,12 +8,16 @@ function Footer() {
             `${todayDate.getFullYear()}`
             );
     };
+    // 
 
     return(
         <footer className="fixed-bottom">
             <ul className="nav justify-content-center border-bottom">
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
+                <a className="nav-link" href="https://github.com/JakobReid/n10469150-cab230" target="_blank">
+                    <img src={GitHub} alt="GitHub" height={40} style={{paddingBottom: 10}}/>
+                </a>
             </ul>
             <p className="text-center" style={{ color: "white", padding: "10px" }}>&copy; Jakob Reid {getCurrentYear()}</p>
         </footer>
